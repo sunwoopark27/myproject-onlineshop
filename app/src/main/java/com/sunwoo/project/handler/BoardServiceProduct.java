397cee69b4efc04a53a55d0708a2dd54faf35f59
sunwoopark27 <sun960127@naver.com>
2021-03-08 02:45:22 +0900

    .

--- app/src/main/java/com/sunwoo/project/handler/BoardServiceProduct.java
+++ app/src/main/java/com/sunwoo/project/handler/BoardServiceProduct.java
@@ -7,20 +7,25 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import com.sunwoo.project.App;
 import com.sunwoo.project.domain.Board;
 
 public class BoardServiceProduct {
 
-  static ArrayList<Board> boardProductList = new ArrayList<>(); //상품 문의
+  static List<Board> boardProductList = new ArrayList<>(); //상품 문의
 
-  BoardAddHandler boardAddHandler = new BoardAddHandler(boardProductList);
-  BoardListHandler boardListHandler = new BoardListHandler(boardProductList);
-  BoardDetailHandler boardDetailHandler = new BoardDetailHandler(boardProductList);
-  BoardUpdateHandler boardUpdateHandler = new BoardUpdateHandler(boardProductList);
-  BoardDeleteHandler boardDeleteHandler = new BoardDeleteHandler(boardProductList);
+  public void menu(String choice) {
 
-  public void menu(String choice) throws CloneNotSupportedException {
+    loadBoards();
+    HashMap<String,Command> commandMap = new HashMap<>();
+
+    commandMap.put("1", new BoardAddHandler(boardProductList));
+    commandMap.put("2", new BoardListHandler(boardProductList));
+    commandMap.put("3", new BoardDetailHandler(boardProductList));
+    commandMap.put("4", new BoardUpdateHandler(boardProductList));
+    commandMap.put("5", new BoardDeleteHandler(boardProductList));
 
     while(true) {
       System.out.printf("[메인 > 게시판 > %s]\n", choice);
@@ -36,21 +41,6 @@ public class BoardServiceProduct {
       System.out.println();
       try {
         switch(command) {
-          case "1" :
-            boardAddHandler.service();
-            break;
-          case "2" :
-            boardListHandler.service();
-            break;
-          case "3" :
-            boardDetailHandler.service();
-            break;
-          case "4" :
-            boardUpdateHandler.service();
-            break;
-          case "5" :
-            boardDeleteHandler.service();
-            break;
           case "0" :
             System.out.println("게시판으로 돌아갑니다.");
             System.out.println();
@@ -58,24 +48,44 @@ public class BoardServiceProduct {
             break;
 
           default :
-            System.out.println("잘못된 메뉴 번호 입니다.");
-            System.out.println();
-
+            Command commandHandler = commandMap.get(command);
+            if(commandHandler == null) {
+              System.out.println("실행할 수 없는 메뉴 번호 입니다.");
+              break;
+            } else {
+              commandHandler.service();
+              break;
+            }
         }
       }catch(Exception e) {
         System.out.println("------------------------------------------------------------------------------");
         System.out.printf("명령어 실행 중 오류 발생: %s - %s\n", e.getClass().getName(), e.getMessage());
         System.out.println("------------------------------------------------------------------------------");
       }
+      saveBoards();
       System.out.println();
     }
   }
 
+  @SuppressWarnings("unchecked")
+  static void loadBoards() {
+    try(ObjectInputStream in = new ObjectInputStream(
+        new BufferedInputStream(
+            new FileInputStream("boardsOfProduct.data")))) {
+
+      boardProductList = (List<Board>) in.readObject();
+      System.out.println("상품 문의 로딩!");
+
+
+    } catch (Exception e) {
+      System.out.println("상품 문의 데이터 로딩 중 오류 발생!");
+    }
+  }
 
   static void saveBoards() {
     try (ObjectOutputStream out = new ObjectOutputStream(
-        new BufferedOutputStream
-        (new FileOutputStream("boardsOfProduct.data")))) { 
+        new BufferedOutputStream(
+            new FileOutputStream("boardsOfProduct.data")))) { 
 
       out.writeObject(boardProductList);
       System.out.println("상품문의가 등록되었습니다.");
@@ -86,19 +96,4 @@ public class BoardServiceProduct {
     }
   }
 
-  @SuppressWarnings("unchecked")
-  static void loadBoards() {
-    try(ObjectInputStream in = new ObjectInputStream(
-        new BufferedInputStream(
-            new FileInputStream("boardsOfProduct.data")))) {
-
-      boardProductList = (ArrayList<Board>) in.readObject();
-      System.out.println("상품 문의 로딩!");
-
-    } catch (Exception e) {
-      System.out.println("상품 문의 데이터 로딩 중 오류 발생!");
-      boardProductList = new ArrayList<>();
-    }
-  }
 }
-
